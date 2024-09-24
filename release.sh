f064650f1c51a1f7f4069e4d3fb438ac09e8dec1
atayeem <tayeemmachine@gmail.com>
2024-09-24 15:00:08 -0400

    Add build script

---
+++ release.sh
@@ -0,0 +1,21 @@
+# This is a janky script to release the website to Github pages.
+# The Github pages repository should be in the same directory as the website source.
+# This script should be invoked in the website source repository.
+
+cd ../BPCI-Robotics.github.io
+git pull
+cd ../website
+
+rm -r ./build/*
+bundle exec middleman build
+cp ../BPCI-Robotics.github.io/.git ./build
+
+rm -rf ../BPCI-Robotics.github.io/*
+cp ./build ../BPCI-Robotics.github.io
+
+cd ../BPCI-Robotics.github.io
+git add .
+git commit -m "Update website"
+git push
+
+echo "Done"
